672f8f1c52c674dd7362e991b9f9183e5cdf0447
PyDiMEHT <154764201+PyDiMEHT@users.noreply.github.com>
2024-01-15 09:10:14 +0400

    up

--- lr2/LR_2/CSA_LR_2-master/src/main/java/com/example/LR_2/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.LR_2.repository;
-
-import com.example.LR_2.models.BookEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<BookEntity, Long> {
-}
